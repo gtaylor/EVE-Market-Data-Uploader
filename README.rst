d91970511f8a29ec6460c557a04eab9489c47251
Greg Taylor <gtaylor@duointeractive.com>
2012-06-26 22:34:13 -0400

    Initial import.

---
+++ README.rst
@@ -0,0 +1,20 @@
+EVE Market Data Uploader (EMDU)
+===============================
+
+:Author: Greg Taylor
+:License: BSD
+
+This project contains a minimalistic EVE Online Market data uploader. It is
+console-based, and aims to be the most cross-platform uploader.
+
+Current state
+-------------
+
+I'm tinkering with it when I feel like it. This may or may never see the
+light of day, but it's a fun toy.
+
+License
+-------
+
+This project, and all contributed code, are licensed under the BSD License.
+A copy of the BSD License may be found in the repository.
---
+++ setup.py
@@ -0,0 +1,44 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+    def find_packages(exclude=None):
+        """
+        Just stub this. If you're packaging, you need setuptools. If
+        you're installing, not so much.
+        """
+        return
+
+import emds
+
+required = [
+    'emds',
+]
+
+scripts = [
+]
+
+setup(
+    name='emdu',
+    version='0.1',
+    description='EVE Market Data Uploader',
+    long_description=open('README.rst').read(),
+    author='Greg Taylor',
+    author_email='gtaylor@gc-taylor.com',
+    url='https://github.com/gtaylor/EVE-Market-Data-Uploader',
+    packages=find_packages(exclude=["tests"]),
+    scripts=scripts,
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=required,
+    license='BSD',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        ),
+    )
